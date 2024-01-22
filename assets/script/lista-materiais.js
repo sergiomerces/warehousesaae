2abf7777067f4502a74f49f931df55838e20bb43
sergiomerces <sergio.merces@gmail.com>
2024-01-22 01:29:04 -0300

    construindo a base de dados

---
+++ assets/script/lista-materiais.js
@@ -0,0 +1,20 @@
+/*
+lista básica de materiais hidráulicos do almoxarifado do SAAE Salto
+Sérgio Merces - atualizado em 22 de janeiro de 2024
+*/
+
+const materiais = [{
+    id: 0,
+    codigo: "54.01.001120",
+    descricao: "Porca para tubete PEAD preto com inserto 3/4",
+    unidade: "Peça",
+    material: "PVC",
+    tamanho: "3/4",
+    almoxarifado: "Central",
+    Corredor: "C01-P01-B01",
+    custo: "R$ 0,00",
+    fornecedor: "Guimarães",
+    compra: "Ata",
+}];
+
+console.table(materiais);
